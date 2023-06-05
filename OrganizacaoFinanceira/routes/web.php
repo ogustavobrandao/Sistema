8a5d2ae13fc9c13e5059b3cc27eabcd57078182c
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2023-06-05 01:56:05 -0300

    Segundo Commit

--- OrganizacaoFinanceira/routes/web.php
+++ OrganizacaoFinanceira/routes/web.php
@@ -21,21 +21,21 @@ Route::get('/', function(){
 
 Route::prefix('entradas')->group(function () {
 
-    Route::get('/', [EntradasController::class, 'index'])->name('entradas.index');
-    Route::get('/create', [EntradasController::class, 'create'])->name('entradas.create');
-    Route::post('/store', [EntradasController::class, 'store'])->name('entradas.store');
-    Route::get('/{id}/edit', [EntradasController::class, 'edit'])->name('entradas.edit');
-    Route::put('/{id}', [EntradasController::class, 'update'])->name('entradas.update');
-    Route::delete('/{id}', [EntradasController::class, 'destroy'])->name('entradas.destroy');
+    Route::get('/', [EntradasController::class, 'index'])->name('entradas.index')->middleware('auth');
+    Route::get('/create', [EntradasController::class, 'create'])->name('entradas.create')->middleware('auth');
+    Route::post('/store', [EntradasController::class, 'store'])->name('entradas.store')->middleware('auth');
+    Route::get('/{id}/edit', [EntradasController::class, 'edit'])->name('entradas.edit')->middleware('auth');
+    Route::put('/{id}', [EntradasController::class, 'update'])->name('entradas.update')->middleware('auth');
+    Route::delete('/{id}', [EntradasController::class, 'destroy'])->name('entradas.destroy')->middleware('auth');
 });
 Route::prefix('ToDoList')->group(function(){
-    Route::get('/', [ListasController::class, 'index'])->name('listas.index');
-    Route::get('/create', [ListasController::class, 'create'])->name('listas.create');
-    Route::post('/store', [ListasController::class, 'store'])->name('listas.store');
-    Route::get('/edit/{id}', [ListasController::class, 'edit'])->name('listas.edit');
-    Route::put('/edit/{id}/put', [ListasController::class, 'update'])->name('listas.update');
-    Route::get('/destroy/{id}',[ListasController::class, 'destroy'])->name('listas.destroy');
-    Route::get('/gerarpdf', [ListasController::class, 'gerar'])->name('listas.gerar');
+    Route::get('/', [ListasController::class, 'index'])->name('listas.index')->middleware('auth');
+    Route::get('/create', [ListasController::class, 'create'])->name('listas.create')->middleware('auth');
+    Route::post('/store', [ListasController::class, 'store'])->name('listas.store')->middleware('auth');
+    Route::get('/edit/{id}', [ListasController::class, 'edit'])->name('listas.edit')->middleware('auth');
+    Route::put('/edit/{id}/put', [ListasController::class, 'update'])->name('listas.update')->middleware('auth');
+    Route::get('/destroy/{id}',[ListasController::class, 'destroy'])->name('listas.destroy')->middleware('auth');
+    Route::get('/gerarpdf', [ListasController::class, 'gerar'])->name('listas.gerar')->middleware('auth');
 });
 
 Auth::routes();
